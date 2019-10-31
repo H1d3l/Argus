dd7910c024c79c3442d642cf5fd925453dfb8cb2
H1d3l <hidels.rios@gmail.com>
2019-10-31 18:14:06 -0300

    sensores ultrassonicos funcionando

--- argus.ino
+++ argus.ino
@@ -1,52 +1,65 @@
 #include <Ultrasonic.h>
 
 //Define os pinos para o trigger e echo do sensor ultrassonico do meio
-#define pino_trigger_middle 4
-#define pino_echo_middle 5
+#define pino_trigger_middle 12
+#define pino_echo_middle 13
+#define pino_trigger_left 10
+#define pino_echo_left 11
+#define pino_trigger_right 8
+#define pino_echo_right 9
 
-Ultrassonic ultrassonic_middle (pino_trigger_middle,pino_echo_middle,40000UL);//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic_middle(pino_trigger_middle,pino_echo_middle);//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic_left(pino_trigger_left,pino_echo_left);//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic_right(pino_trigger_right,pino_echo_right);//Inicializa o sensor nos pinos definidos acima
+
+
 
 int distance_obstacle = 30; //distância para o robô parar e recalcular o melhor caminho
 
 void setup() {
      
   Serial.begin(9600);// inicializa a comunicação serial para mostrar dados
-  pinMode(13, OUTPUT);  // define pino 13 como saida  
-  pinMode(pino_trigger_middle, OUTPUT); //define o pino TRIG como saída
-  pinMode(pino_echo_middle, INPUT);  //define o pino ECHO como entrada 
+  pinMode(4, OUTPUT);  // define pino 13 como saida  
   
 
 }
 
+
 void loop() {
-  turn_on_led()
-  main()
+  turn_on_led();
+  int distance_middle = read_ultrasonic_middle();
+  int distance_left = read_ultrasonic_left();
+  int distance_right = read_ultrasonic_right();
+
+
+  Serial.print("Distance in CM meio: ");
+  Serial.println(distance_middle);
+  Serial.print("Distance in CM esquerda: ");
+  Serial.println(distance_left);
+  Serial.print("Distance in CM direita: ");
+  Serial.println(distance_right);
 
 }
 
 void turn_on_led(){
-  digitalWrite(13, HIGH);// Ativa o pino 13 (colocando 5v nele)  
-  delay(1000);// Aguarda 1 segundo
-  digitalWrite(13, LOW);// Desliga o pino 12
-  delay(1000);// Aguarda 1 segundo
+  digitalWrite(4, HIGH);// Ativa o pino 13 (colocando 5v nele)  
+  delay(500);// Aguarda 1 segundo
+  digitalWrite(4, LOW);// Desliga o pino 12
+  delay(500);// Aguarda 1 segundo
   
 }
-
-void main(){
-  int distance = read_distance_cm() // Ler o sensor de distância  
-  Serial.print("distancia em cm: "); 
-  Serial.println(distancia);
-
-  if(distance>distance_obstacle){
-    walk forward()//implementar andar para frente    
-  }
-  else{
-      rotacao_Parado();  //implementar parar o robô  
-      posicionaCarroMelhorCaminho(); //implementar calcula o melhor caminho    
-        main();
-  }
+int read_ultrasonic_middle(){
+  int distance = ultrasonic_middle.read(CM);
+  return distance;
+  
 }
-
-int read_distance_cm(){
-  return ultrassonic_middle.read(CM)
+int read_ultrasonic_left(){
+  int distance = ultrasonic_left.read(CM);
+  return distance;
+  
+}
+int read_ultrasonic_right(){
+  int distance = ultrasonic_right.read(CM);
+  return distance;
+  
 }
