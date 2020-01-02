23987271df10b59f7d3d1c9cb273478d968b7474
H1d3l <hidels.rios@gmail.com>
2020-01-02 11:13:26 -0300

    movimentacao argus

--- argus/argus.ino
+++ argus/argus.ino
@@ -8,20 +8,20 @@
 #define pino_echo_middle 13
 #define pino_echo_left 11
 
-#define pino_enA A2 //pino enA da ponte h l298
-#define pino_enB A3 //pino enB da ponte h l298
-#define pino_in1 3 //pino in1 da ponte h l298 
-#define pino_in2 4 //pino in2 da ponte h l298
-#define pino_in3 5 //pino in3 da ponte h l298
-#define pino_in4 6 //pino in4 da ponte h l298
+#define pino_enA 3 //pino enA da ponte h l298
+#define pino_enB 4 //pino enB da ponte h l298
+#define pino_in1 51 //pino in1 da ponte h l298 
+#define pino_in2 50 //pino in2 da ponte h l298
+#define pino_in3 49 //pino in3 da ponte h l298
+#define pino_in4 48 //pino in4 da ponte h l298
 
 Ultrasonic ultrasonic_middle(pino_trigger_middle,pino_echo_middle);//Inicializa o sensor nos pinos definidos acima
 Ultrasonic ultrasonic_left(pino_trigger_left,pino_echo_left);//Inicializa o sensor nos pinos definidos acima
 Ultrasonic ultrasonic_right(pino_trigger_right,pino_echo_right);//Inicializa o sensor nos pinos definidos acima
 
 int distance_obstacle = 30; //distância para o robô parar e recalcular o melhor caminho
-int motor_speed_one = 80;
-int motor_speed_two = 95;
+int motor_speed_one = 250;
+int motor_speed_two = 250;
 
 void setup() {
      
@@ -40,10 +40,25 @@ void setup() {
 
 void loop() {
   turn_on_led();
-  go_forward();
+
   int distance_middle = read_ultrasonic_middle();
   int distance_left = read_ultrasonic_left();
   int distance_right = read_ultrasonic_right();
+
+  if (distance_middle >= 40){
+    go_forward();
+  }else{
+    if(distance_right > distance_left){
+      turn_right();
+    }
+    
+    else if(distance_left > distance_right){
+      turn_left();
+    }
+    else{
+      return_back();
+    }
+  }
 
 
   Serial.print("Distance in CM meio: ");
@@ -56,15 +71,66 @@ void loop() {
 }
 void go_forward()
 {
-Serial.println("Robô: para o alto e avante");
-digitalWrite(pino_in1, HIGH);                          //Configurar a ponte h 
-digitalWrite(pino_in2, LOW);
+Serial.println("indo para frente");
+digitalWrite(pino_in1, HIGH);//MOTOR ESQUERDO 
+digitalWrite(pino_in2, LOW);//MOTOR DIREITO
+digitalWrite(pino_in3, LOW);
+digitalWrite(pino_in4, HIGH);
+analogWrite(pino_enA, motor_speed_one);              // Defina a velocidade do motor Um
+analogWrite(pino_enB, motor_speed_two);            // Defina a velocidade do motor Dois                         
+}
+void go_back()
+{
+Serial.println("indo para trás");
+digitalWrite(pino_in1, LOW);//MOTOR ESQUERDO 
+digitalWrite(pino_in2, HIGH);//MOTOR DIREITO
 digitalWrite(pino_in3, HIGH);
 digitalWrite(pino_in4, LOW);
 analogWrite(pino_enA, motor_speed_one);              // Defina a velocidade do motor Um
 analogWrite(pino_enB, motor_speed_two);            // Defina a velocidade do motor Dois                         
 }
 
+void pause()
+{
+Serial.println("Robô: Parar ");
+digitalWrite(pino_in1, LOW);                        
+digitalWrite(pino_in2, LOW);
+digitalWrite(pino_in3, LOW);
+digitalWrite(pino_in4, LOW);
+delay(100);   //aguarda um tempo
+}
+void turn_right()
+{
+Serial.println("Robô: Direita ");
+digitalWrite(pino_in1, LOW);                           //Configurar a ponte h 
+digitalWrite(pino_in2, HIGH);
+digitalWrite(pino_in3, HIGH);
+digitalWrite(pino_in4, LOW);
+delay(300);                                       //aguarda um tempo
+analogWrite(pino_enA, motor_speed_one);              // Defina a velocidade do motor Um
+analogWrite(pino_enB, motor_speed_two);            // Defina a velocidade do motor Dois                         
+}
+void turn_left()
+{
+Serial.println("Robô: Esquerda ");                
+digitalWrite(pino_in1, HIGH);                          //Configurar a ponte h 
+digitalWrite(pino_in2, LOW);
+digitalWrite(pino_in3, HIGH);
+digitalWrite(pino_in4, LOW);
+delay(300);                                       //aguarda um tempo
+analogWrite(pino_enA, motor_speed_one);              // Defina a velocidade do motor Um
+analogWrite(pino_enB, motor_speed_two);            // Defina a velocidade do motor Dois                         
+}
+void return_back() {    
+Serial.println("Robô: Girar ");      
+digitalWrite(pino_in1, HIGH);                          //Configurar a ponte h 
+digitalWrite(pino_in2, LOW);
+digitalWrite(pino_in3, HIGH);
+digitalWrite(pino_in4, LOW);
+delay (700);                                      //aguarda um tempo
+analogWrite(pino_enA, motor_speed_one);              // Defina a velocidade do motor Um
+analogWrite(pino_enB, motor_speed_two);            // Defina a velocidade do motor Dois                         
+}
 void turn_on_led(){
   digitalWrite(2, HIGH);// Ativa o pino 13 (colocando 5v nele)  
   delay(500);// Aguarda 1 segundo
